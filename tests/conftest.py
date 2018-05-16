ce3f6f70b48e2400b97ac77cec978784a81d9a99
Martin van Harmelen <Martin@vanharmelen.com>
2018-05-16 13:35:12 +0200

    Added another test config combination

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,4 +5,5 @@ from hypothesis import settings
 # settings.register_profile("default", deadline=1000, max_shrinks=100)
 # settings.register_profile("default", deadline=None, max_shrinks=100)
 settings.register_profile("default", max_examples=50, deadline=None, max_shrinks=100)
+# settings.register_profile("default", max_examples=50, deadline=200, max_shrinks=100)
 settings.load_profile(os.getenv(u'HYPOTHESIS_PROFILE', 'default'))
